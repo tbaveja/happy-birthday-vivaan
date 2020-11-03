f8bd4d591ca5e05ed8cca4cb7aca68bb191ed4ea
tbaveja <tbaveja@gmail.com>
2020-11-04 02:05:30 +0530

    Add files via upload

---
+++ photo/data.js
@@ -0,0 +1,52 @@
+var data = [];
+
+var dataStr = "1.DeathNote<br>\
+<br>\
+Japanese Cartoon<br>\
+<br>\
+<br>\
+2.FateUBW<br>\
+<br>\
+Japanese Cartoon<br>\
+<br>\
+<br>\
+3.FateZero<br>\
+<br>\
+Japanese Cartoon<br>\
+<br>\
+<br>\
+4.GoneGirl<br>\
+<br>\
+American Movie<br>\
+<br>\
+<br>\
+5.Shameless<br>\
+<br>\
+American TV Series<br>\
+<br>\
+<br>\
+6.theDarkKnight<br>\
+<br>\
+American Movie<br>\
+<br>\
+<br>\
+7.ToyStory<br>\
+<br>\
+American Movie<br>\
+"
+var d = dataStr.split("<br><br><br>");
+for(var i = 0; i<d.length; i++){
+  var c1 = d[i].split("<br><br>");
+  data.push({
+    img: c1[0]+ ".jpg",
+    caption: c1[0].split(".")[1],
+    desc: c1[1]
+  });
+}
+
+
+  
+
+$('.giftbox div').click(function() {
+	console.log("test");
+});
